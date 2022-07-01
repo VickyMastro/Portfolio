8b46d46704d91cecd7f71da19e86ca285eb3601b
VickyMastro <vicmastroianni1702@gmail.com>
2022-06-30 21:02:58 -0300

    Add # so that it doesn't refresh

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,10 +7,10 @@
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item href="presentation">About Me</b-nav-item>
-        <b-nav-item href="skills">Skills</b-nav-item>
-        <b-nav-item href="projects">Projects</b-nav-item>
-        <b-nav-item href="contact">Contact Me</b-nav-item>
+        <b-nav-item href="#presentation">About Me</b-nav-item>
+        <b-nav-item href="#skills">Skills</b-nav-item>
+        <b-nav-item href="#projects">Projects</b-nav-item>
+        <b-nav-item href="#contact">Contact Me</b-nav-item>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
