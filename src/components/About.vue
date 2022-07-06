d6d714505c50819055ca48705023ddc21f53aa1d
Victoria Mastroianni <vicmastroianni1702@gmail.com>
2022-07-06 15:25:18 -0300

    add content in about me

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,15 +1,35 @@
 <template>
   <div>
-    probando About
+    <b-container fluid>
+      <b-row class="title-about">
+        <b-col sm="12"> About Me </b-col>
+      </b-row>
+      
+      <hr style="width: 80%; margin-left: 10%;"/>
+
+      <b-row align-h="center">
+        <b-col sm="6">
+          <div class="my-4">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore
+            ullam maiores sequi excepturi? Ullam ducimus deserunt maxime ipsa
+            possimus optio vel rerum consectetur eos hic, error reiciendis alias
+            impedit delectus!
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'About',
-}
+  name: "About",
+};
 </script>
 
 <style scoped>
-
+.title-about {
+  font-size: 35px;
+  color: #1aac9b;
+}
 </style>
