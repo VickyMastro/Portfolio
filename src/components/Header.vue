d7d97c201c4c1c1337f708a6f3417346fe2d4801
VickyMastro <vicmastroianni1702@gmail.com>
2022-07-14 15:15:58 -0300

    Change title of about

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,31 +1,32 @@
 <template>
-  <div>
-  <b-navbar toggleable="lg" type="dark" class="navbar-container px-3">
-    <b-navbar-brand href="#">VickyMastro</b-navbar-brand>
+    <b-navbar
+      toggleable="lg"
+      type="dark"
+      class="navbar-container px-3"
+    >
+      <b-navbar-brand href="#">VickyMastro</b-navbar-brand>
 
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-    <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav>
-        <b-nav-item href="#presentation">About Me</b-nav-item>
-        <b-nav-item href="#skills">Skills</b-nav-item>
-        <b-nav-item href="#projects">Projects</b-nav-item>
-        <b-nav-item href="#contact">Contact Me</b-nav-item>
-      </b-navbar-nav>
-    </b-collapse>
-  </b-navbar>
-</div>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#presentation">Presentation</b-nav-item>
+          <b-nav-item href="#skills">Skills</b-nav-item>
+          <b-nav-item href="#projects">Projects</b-nav-item>
+          <b-nav-item href="#contact">Contact Me</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
 </template>
 
 <script>
 export default {
-    name: 'Header',
+  name: 'Header',
 }
 </script>
 
 <style scoped>
 .navbar-container{
   background-color: #1aac9b;
-
 }
 </style>
