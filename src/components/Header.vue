69b0c8ac94851ba44a254a35e54d37af69b42ae3
Victoria Mastroianni <vicmastroianni1702@gmail.com>
2022-07-14 22:00:15 -0300

    use scroll-to

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,18 +2,21 @@
     <b-navbar
       toggleable="lg"
       type="dark"
+      sticky
       class="navbar-container px-3"
     >
-      <b-navbar-brand href="#">VickyMastro</b-navbar-brand>
+      <b-navbar-brand>
+        <a href="/" style='text-decoration: none; color: #fff;'>VickyMastro</a>
+      </b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item href="#presentation">Presentation</b-nav-item>
-          <b-nav-item href="#skills">Skills</b-nav-item>
-          <b-nav-item href="#projects">Projects</b-nav-item>
-          <b-nav-item href="#contact">Contact Me</b-nav-item>
+          <b-nav-item v-scroll-to="'#presentation'">Presentation</b-nav-item>
+          <b-nav-item v-scroll-to="'#skills'">Skills</b-nav-item>
+          <b-nav-item v-scroll-to="'#projects'">Projects</b-nav-item>
+          <b-nav-item v-scroll-to="'#contact'">Contact Me</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
