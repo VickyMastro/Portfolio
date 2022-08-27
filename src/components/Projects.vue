b16b9be26699e13d7c38b2f12997bb99fb117f78
VickyMastro <vicmastroianni1702@gmail.com>
2022-08-27 13:48:09 -0300

    add first project: app ToDo

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -10,22 +10,31 @@
       <b-row align-h="around">
         <b-col sm="8" md="4" class="d-flex justify-content-around">
           <b-card
-            title="Card "
-            img-src="https://picsum.photos/600/300/?image=20"
-            img-alt="Image"
+            title="ToDo App"
+            :img-src="require('@/assets/toDo-App.png')"
+            img-alt="ToDo project"
             img-top
             tag="article"
             style="max-width: 20rem"
             class="card-container my-3"
           >
             <b-card-text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              App for notes and toDos with authentication, made with Vue, Firebase and Bootstrap
             </b-card-text>
 
-            <div class="d-flex justify-content-around">
-              <b-button href="#" class="project-buttons">GitHub</b-button>
-              <b-button href="#" class="project-buttons">Page</b-button>
+            <div class="d-flex justify-content-around mt-5">
+              <b-button
+                href="https://github.com/VickyMastro/Vue-ToDo-App"
+                class="project-buttons"
+                target="_blank"
+                >GitHub</b-button
+              >
+              <b-button
+                href="https://vickymastro.github.io/Vue-ToDo-App/"
+                class="project-buttons"
+                target="_blank"
+                >Page</b-button
+              >
             </div>
           </b-card>
         </b-col>
@@ -103,8 +112,8 @@ export default {
   box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
 }
 
-.project-buttons:focus , 
-.project-buttons:hover{
+.project-buttons:focus,
+.project-buttons:hover {
   background-color: #1aac9b;
   box-shadow: none;
 }
